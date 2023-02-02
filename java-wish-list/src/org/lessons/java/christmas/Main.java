d5dcf51c7aaa87cf0cc28353bdeb9d57ca39f98c
Rossiitaa <rosscuotto1998@gmail.com>
2023-02-02 15:08:07 +0100

    ex. completed

--- java-wish-list/src/org/lessons/java/christmas/Main.java
+++ java-wish-list/src/org/lessons/java/christmas/Main.java
@@ -1,5 +1,39 @@
 package org.lessons.java.christmas;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		boolean done = false;
+		List<String> wishlist = new ArrayList<>();
+
+		while (!done) {
+			System.out.print("Write a wish to be placed on the wish list: ");
+			String item = s.nextLine();
+
+			wishlist.add(item);
+			System.out.println("-------------------");
+			System.out.println("You have " + wishlist.size() + " element on your list");
+
+			System.out.println("-------------------");
+
+			System.out.println("Do you want to add more wishes? (y/n)");
+			String userChoice = s.nextLine();
+
+			if (userChoice.equals("n")) {
+				done = true;
+				wishlist.sort(null);
+				System.out.println("Here are all the wishes you have put on your list: ");
+
+				for (String element : wishlist) {
+					System.out.println("- " + element);
+				}
+			}
+		}
+		s.close();
+	}
 }
